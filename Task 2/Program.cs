b632b76158c6c38be86943d3af9cd335c7e9516a
MartinLazarovBG <green_sky321@abv.bg>
2017-10-02 15:45:24 +0300

    Add files via upload

---
+++ Task 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}");
+        }
+    }
+}
